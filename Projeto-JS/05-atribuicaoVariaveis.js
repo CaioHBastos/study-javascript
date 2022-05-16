5ce880d1fe64990a30e200c8313f7c6ad40987f2
CaioHBastos <caio.cbastos@hotmail.com>
2022-05-16 08:31:53 -0300

    Js

---
+++ Projeto-JS/05-atribuicaoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trablalhando com atribuição de variáveis");
+
+const idade = 24;
+const primeiroNome = "Caio";
+const sobrenome = "Bastos"
+
+//console.log(nome + "" + sobrenome);
+//console.log(nome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
